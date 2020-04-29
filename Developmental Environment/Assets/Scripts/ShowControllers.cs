12c6046d69666666530cd9babd3676e117b87bb3
Rachel Wong <rachel.wong@hotmail.ca>
2020-04-29 12:45:15 -0600

    Updated ShowControllers Script

---
+++ Developmental Environment/Assets/Scripts/ShowControllers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+// Script to allow controllers to be shown during gameplay
+
+public class ShowControllers : MonoBehaviour
+{
+
+    public bool showController = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var hand in Player.instance.hands)
+        {
+            if(showController)
+            {
+                hand.ShowController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController); // set hands to hold controller properly
+            }
+
+            else
+            {
+                hand.HideController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithoutController);
+            }
+        }
+    }
+}
